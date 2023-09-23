774f06ba56302e9474c381b007c872528de8ae78
Joal1291 <jordan.albert@github.com>
2023-09-23 16:44:37 +0200

    ajout des accés au page via les button, ajout option quit direct via le jeux

--- Hangman_game_pygame/main_hangman.py
+++ Hangman_game_pygame/main_hangman.py
@@ -1,15 +1,15 @@
-import pygame as py
+import pygame, sys
 import time
 import random
 import funct_title_screen as ts
 
 
-py.init()
-py.display.set_caption('Hangman Game')
+pygame.init()
+pygame.display.set_caption('Hangman Game')
 
 screen_resolution = (1000, 656)
-flags = py.DOUBLEBUF | py.HWSURFACE
-screen = py.display.set_mode(screen_resolution, flags)
+flags = pygame.DOUBLEBUF | pygame.HWSURFACE |pygame.NOFRAME
+screen = pygame.display.set_mode(screen_resolution, flags)
 
 #----------------------------------------- CLASS
 class Button():
@@ -24,58 +24,111 @@ class Button():
     
     def check_position_input(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            print(position)
+            return True
+        else:
+            return False
 #--------------------------------------------------------------------------------------------------------------------------
 
-
-#----------------------------------------- Function traveller
-def title_screen():
-    py.display.set_caption('Hangman Game - Main Menu')
-    # game = False
-    # record = False
-    ts.title_screen_animation(screen)
-
-    button_play_surface = py.image.load("image/image_play.png")
-    button_play_surface = py.transform.scale(button_play_surface, (250, 100))
-
-    button_play = Button(button_play_surface, 250, 550)
-
-    button_record_surface = py.image.load("image/image_record.png")
-    button_record_surface = py.transform.scale(button_record_surface, (250,140))
-
-    button_record = Button(button_record_surface, 750, 565)
+#----------------------------------------- Record Page
+def record_page():
 
     while True:
-        background_surface = py.image.load("image_anim_title_screen/output_image_208.png")
-        name_game_surface = py.image.load("image/namegamewhite.png")
+        mouse_position = pygame.mouse.get_pos()
 
-        button_play_surface = py.image.load("image/image_play.png")
-        button_play_surface = py.transform.scale(button_play_surface, (250, 100))
+        background_surface = pygame.image.load("image_anim_title_screen/output_image_18.png")
+
+        record_title_page_image = pygame.image.load("image/record_page_titlte.png")
+
+        button_back_image = pygame.image.load("image/image_back.png")
+        button_back_image = pygame.transform.scale(button_back_image, (250,100))
+        button_back = Button(button_back_image, 850, 600)
+        
+        screen.blit(background_surface, (0, 0))
+        button_back.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_back.check_position_input(mouse_position):
+                    title_screen(1)
+
+        pygame.display.update()
+
+#---------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------- Play Page
+def play_page():
+
+    while True:
+        mouse_position = pygame.mouse.get_pos()
+
+        background_surface = pygame.image.load("image_anim_title_screen/output_image_160.png")
+
+        button_back_image = pygame.image.load("image/image_back.png")
+        button_back_image = pygame.transform.scale(button_back_image, (250,100))
+        button_back = Button(button_back_image, 850, 600)
+        
+        screen.blit(background_surface, (0, 0))
+        button_back.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if button_back.check_position_input(mouse_position):
+                    title_screen(1)
+
+        pygame.display.update()
+
+#---------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------- Function traveller
+def title_screen(x):
+    pygame.display.set_caption('Hangman Game - Main Menu')
+    # game = False
+    # record = False
+    if x == 0:
+        ts.title_screen_animation(screen)
+
+    while True:
+        mouse_position = pygame.mouse.get_pos()
+
+        background_surface = pygame.image.load("image_anim_title_screen/output_image_208.png")
+        name_game_surface = pygame.image.load("image/namegamewhite.png")
+
+        button_play_surface = pygame.image.load("image/image_play.png")
+        button_play_surface = pygame.transform.scale(button_play_surface, (250, 100))
         button_play = Button(button_play_surface, 250, 550)
 
-        button_record_surface = py.image.load("image/image_record.png")
-        button_record_surface = py.transform.scale(button_record_surface, (250,140))
+        button_record_surface = pygame.image.load("image/image_record.png")
+        button_record_surface = pygame.transform.scale(button_record_surface, (250,140))
         button_record = Button(button_record_surface, 750, 565)
 
-        button_quit_surface = py.image.load("image/image_quit.png")
-        button_quit_surface = py.transform.scale(button_quit_surface, (150, 100))
+        button_quit_surface = pygame.image.load("image/image_quit.png")
+        button_quit_surface = pygame.transform.scale(button_quit_surface, (150, 100))
         button_quit = Button(button_quit_surface, 500, 600)
         
-        for event in py.event.get():
-            if event.type == py.QUIT:
-                py.quit()
-            if event.type == py.MOUSEBUTTONDOWN:
-                button_play.check_position_input(py.mouse.get_pos())
-                button_record.check_position_input(py.mouse.get_pos())
-                button_quit.check_position_input(py.mouse.get_pos())
+        # screen.blit(background_surface, (0, 0))
+        # screen.blit(name_game_surface, (250, 200))
+        # button_play.update()
+        # button_record.update()
+        # button_quit.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if button_record.check_position_input(mouse_position):
+                    record_page()
+                if button_quit.check_position_input(mouse_position):
+                    pygame.quit()
+                    sys.exit()
+                if button_play.check_position_input(mouse_position):
+                    play_page()
 
         screen.blit(background_surface, (0, 0))
         screen.blit(name_game_surface, (250, 200))
         button_play.update()
         button_record.update()
         button_quit.update()
-        py.display.update()
-        py.display.flip()
+        pygame.display.update()
+        # pygame.display.flip()
 
-title_screen()
+title_screen(0)
 #-----------------------------------------------------------------------------------------
